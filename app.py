d412f123feb52e8ada57b43242820492ca82ce82
NelsonCoder525 <nelsonr525@hotmail.com>
2026-03-11 20:59:41 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+import pandas as pd
+import requests
+
+app = Flask(__name__)
+
+@app.route('/<uf>/<cidade>/<logradouro>')
+def index(uf, cidade, logradouro):
+   
+   try:
+        uf = uf.upper()
+        
+        link = f"https://viacep.com.br/ws/{uf}/{cidade}/{logradouro}/json/"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        
+        tabela = pd.DataFrame(dic_requisicao)
+        tabela = tabela[["cep", "logradouro", "complemento", "bairro", "localidade", "uf"]]
+        return tabela.to_html()
+   except Exception as erro:
+        return f"Erro: {erro}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
